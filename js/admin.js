f671711560cd2b9ef804a5da70285d97cccd91de
arielMerino <ariel.merino@usach.cl>
2014-10-31 15:46:51 -0300

    login administrador agregado

---
+++ js/admin.js
@@ -0,0 +1,2 @@
+document.getElementById("menu").style.display = "none";
+document.getElementById("menu-lateral").style.display ="none";
---
+++ js/cotiza.js
@@ -0,0 +1,9 @@
+document.getElementById('cn').className="btn btn-default active";
+$('#sandbox-container .input-group.date').datepicker({
+                format: "dd/mm/yyyy",
+                weekStart: 1,
+                startDate: "01/01/2014",
+                language: "es",
+                keyboardNavigation: false,
+                todayHighlight: true
+            });
